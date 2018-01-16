258faf7a710d38e5b4962a73a75a082cd8158450
Kris McGinnes <kmcginnes@me.com>
2018-01-16 15:30:16 -0600

    Example app that launches child process folder browser

---
+++ src/HostProcess/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+
+namespace HostProcess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var processStartInfo = new ProcessStartInfo
+            {
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardInput = true,
+                FileName = "OpenFileDialogProcess.exe"
+            };
+
+            var process = new Process
+            {
+                StartInfo = processStartInfo,
+                EnableRaisingEvents = true,
+            };
+
+            var output = new StringBuilder();
+
+            process.Start();
+            process.BeginOutputReadLine();
+
+            process.OutputDataReceived += (sender, eventArgs) => { output.AppendLine(eventArgs.Data); };
+
+            process.WaitForExit();
+            process.CancelOutputRead();
+
+            Console.WriteLine($"User chose the following file path: {output}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ src/OpenFileDialogProcess/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace OpenFileDialogProcess
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var fileDialog = new OpenFileDialog
+            {
+                InitialDirectory = "c:\\",
+                Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*",
+                FilterIndex = 2,
+                RestoreDirectory = true
+            };
+
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    Console.WriteLine(fileDialog.FileName);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+    }
+}
